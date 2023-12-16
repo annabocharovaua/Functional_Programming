42a7b12e2e20c2c7eb14398aca41d73e6fbc2323
Anna Bocharova <120383629+annabocharovaua@users.noreply.github.com>
2023-12-16 21:29:04 +0200

    update task5

--- problems/task6/Main.hs
+++ problems/task6/Main.hs
@@ -6,23 +6,17 @@ import qualified Data.Text.Lazy.IO as TLIO
 
 main :: IO ()
 main = do
-  -- Створення XML-документу
-  let xmlDocument = Document (Prologue [] Nothing []) rootElement []
-
-  -- Запис XML у файл
-  TLIO.writeFile "example.xml" $ renderText def xmlDocument
-
-  -- Створення DTD-визначення
-  let dtd = DTD
+   let xmlDocument = Document (Prologue [] Nothing []) rootElement []
+   TLIO.writeFile "example.xml" $ renderText def xmlDocument
+   let dtd = DTD
         [ DTDElement "root" (DTDEmpty
                               (Just $ DTDEntity "data" "CDATA" Nothing)
                               (Just $ DTDEntity "attr" "CDATA" Nothing))
         ]
 
-  -- Запис DTD-визначення у файл
+
   TLIO.writeFile "example.dtd" $ renderText def dtd
 
--- Створення кореневого елементу XML
 rootElement :: Element
 rootElement =
   Element
